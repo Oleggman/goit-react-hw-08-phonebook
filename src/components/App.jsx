b93318db3bc20453951f8f1f15906070bef0a0d2
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-11-18 14:32:27 +0200

    Create private and restricted routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,35 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { RiContactsBook2Fill } from 'react-icons/ri';
-import { ContactsForm } from './ContactsForm/ContactsForm'
-import { ContactsBook } from './ContactsBook/ContactsBook'
-import { Filter } from './Filter/Filter'
-import { MainContainer, AppTitle, ContactsList, ContactsTitle } from './App.styled';
-import { fetchContacts } from 'redux/contacts/operations';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import { Home } from 'pages/Home';
 import { Contacts } from 'pages/Contacts';
 import { Register } from 'pages/Register';
 import { Login } from 'pages/Login';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element ={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/contacts' element={<Contacts />} />
+        <Route
+          path='/register'
+          element={
+            <RestrictedRoute component={<Register />} redirectTo='/contacts' />
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <RestrictedRoute component={<Login />} redirectTo='/contacts' />
+          }
+        />
+        <Route
+          path='/contacts'
+          element={
+            <PrivateRoute component={<Contacts />} redirectTo='/login' />
+          }
+        />
       </Route>
     </Routes>
   )
