5636dff33c98632fbf3d2e886fdb38db09dcfd02
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-11-18 13:39:42 +0200

    Update store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const theme = {}
 
@@ -12,8 +13,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <App />
           <GlobalStyle />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
