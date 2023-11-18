0b03453fa17aa8b8dcf96bf5d1cc96d5f464aa3d
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-11-18 13:23:08 +0200

    Create auth operations

---
+++ src/redux/auth/slice.js
@@ -0,0 +1,18 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {
+
+  }
+})
+
+export const authReducer = authSlice.reducer;
